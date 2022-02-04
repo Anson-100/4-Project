3b46609f43c739cc37c85aab3a6dd3618dc5c7e7
anson <ansonhunnell@gmail.com>
2022-02-04 12:26:18 -0500

    .

--- app.js
+++ app.js
@@ -5,21 +5,23 @@ const navSlide = () => {
 
 
     
-    burger.addEventListener('click',() => {
+    burger.addEventListener('click', () => {
         //Toggle Nav    
         nav.classList.toggle('nav-active');
     
         //Animate Links
     navLinks.forEach((link, index) => {
-        if (link.style.animation) {
+        if(link.style.animation) {
             link.style.animation = '';
         } else {
-        
-            link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s';
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 2}s`;
             console.log(index / 7);
         }
         });
+        //Burger Animation
+        burger.classList.toggle('toggle');
     });
+
 }
 
 navSlide();
